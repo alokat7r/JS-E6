a056e0303c60a8e90d65469f09fe155df9c935e0
Alok Ranjan Singh <007alokranjan@gmail.com>
2019-02-11 08:59:32 +0530

    Added Code Of Email Sending

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+/**
+ * This code helps automate the export of the files in this dir, now we don't need
+ * to always mention each new  files in this index file to make it accessable through out
+ * the project through module import.
+ */
+'use strict';
+const fs = require('fs');
+let files = fs.readdirSync(`${__dirname}`,{encoding:'utf8',withFileTypes:false});
+let exportJSON = files.reduce((acc,element)=>{
+    let fileOrDirWithoutExtension = element.split('.')[0];
+    if(fileOrDirWithoutExtension != "index" && fileOrDirWithoutExtension != "")
+    return Object.assign(acc,{[fileOrDirWithoutExtension]:require(`./${fileOrDirWithoutExtension}`)});
+    return acc; 
+} ,{});
+module.exports = exportJSON;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
+const dotenv = require('dotenv').config();
+const AWS = require('aws-sdk');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESSKEY,
+  secretAccessKey: process.env.AWS_SECRETKEY,
+  region: process.env.AWS_SES_SERVICE
+});
+
+const SES = new AWS.SES({
+  apiVersion: '2010-12-01',
+});
+
+
+/**
+ * 
+ * @param {*} toMail 
+ * @param {*} generatedRandomNumber 
+ * @description Send Welcome Email With Passcode
+*/
+router.get('/:mailTo', function(req, res, next) {
+  let {mailTo} = req.params;
+  let {message,subject} = req.query;
+  let from = process.env.ACCOUNT_VERIFICATION_EMAIL_FROM;
+  let emailParam = {
+                  Source: from,
+                  Destination: {
+                      ToAddresses: [mailTo]
+                  },
+                  Message: {
+                      Body: {
+                          Html: {
+                              Charset: "UTF-8",
+                              Data: message
+                          },
+                      },
+                      Subject: {
+                          Charset: "UTF-8",
+                          Data: subject
+                      }
+                  }
+  };
+  let emailResObject = await SES.sendEmail(emailParam);
+  let emailResponse =  await emailResObject.promise();
+  res.json({
+    success:true,
+    message:emailResponse
+  });
 });
 
 module.exports = router;
